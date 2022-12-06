d1ee29d6a7d1ef86653a44f72ebc10d4341dde30
devcoderoad <defrian.yarfi@gmail.com>
2022-12-06 13:29:09 +0700

    chore: init

---
+++ nuxt.config.js
@@ -0,0 +1,84 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxtjs-i18n-content',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    // https://i18n.nuxtjs.org/
+    '@nuxtjs/i18n',
+  ],
+
+  // i18n languages setup
+  i18n: {
+    locales: ['en', 'fr', 'es'],
+    defaultLocale: 'en',
+    vueI18n: {
+      fallbackLocale: 'en',
+      messages: {
+        en: {
+          title: 'Welcome',
+          welcome: 'Welcome',
+          home: 'Home',
+          Profile: 'Profile English',
+        },
+        fr: {
+          title: 'Welcome fr',
+          welcome: 'Bienvenue fr',
+          home: 'Home fr',
+          Profile: 'Profile French',
+        },
+        es: {
+          title: 'Welcome es',
+          welcome: 'Bienvenido es',
+          home: 'Home es',
+          Profile: 'Profile Estonia',
+        },
+      },
+    },
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ pages/profile.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <Tutorial />
+    <p>
+      <nuxt-link :to="localePath('index')">{{ $t('home') }}</nuxt-link>
+      <nuxt-link :to="localePath('/')">{{ $t('home') }}</nuxt-link>
+      <nuxt-link :to="localePath('index', 'en')">Homepage in English</nuxt-link>
+      <nuxt-link :to="localePath('/app/profile')"
+        >Route by path to: {{ $t('Profile') }}</nuxt-link
+      >
+      <nuxt-link :to="localePath('app-profile')"
+        >Route by name to: {{ $t('Profile') }}</nuxt-link
+      >
+      <!-- <nuxt-link
+      :to="
+        localePath({ name: 'category-slug', params: { slug: category.slug } })
+      "
+    >
+      {{ category.title }}
+    </nuxt-link> -->
+      <!-- It's also allowed to omit 'name' and 'path'. -->
+      <!-- <nuxt-link :to="localePath({ params: { slug: 'ball' } })">{{
+      category.title
+    }}</nuxt-link> -->
+    </p>
+    <footer>
+      <p>
+        <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
+        <nuxt-link :to="switchLocalePath('fr')">Français</nuxt-link>
+        <nuxt-link :to="switchLocalePath('es')">Estonia</nuxt-link>
+      </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfilePage',
+}
+</script>
